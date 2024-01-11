ef4adc090388de75b28c916f8ed2356afac7602f
Himang Garg <130616649+himangg@users.noreply.github.com>
2024-01-11 23:01:14 +0530

    Add files via upload

---
+++ AP_PROJECT/src/main/java/com/example/ap_project/CharacterAbstractClass.java
@@ -0,0 +1,12 @@
+package com.example.ap_project;
+
+import javafx.scene.image.Image;
+
+public class CharacterAbstractClass {
+    public static Character getInstance(){
+        return null;
+    };
+    public static Image getMyimage(){
+        return null;
+    }
+}
